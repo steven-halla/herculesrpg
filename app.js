8b317f186a4d59852b90010b28c6a438ea2a4caf
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-08-27 12:50:07 -0700

    "created player and monster objects"

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
-function myFunction() {
-  alert("Hello! I am an alert box!");
+function welcomeAlert() {
+  alert("Welcome to the start of your heroic journey ");
 }
-fsafsdaf
-myFunction();
+welcomeAlert();
+
+let hercules = {
+   "health" : 12,
+   "attackPower" : 5,
+   "swordSlash" : 10,
+  "falconPunch" : 11
+};
+
+let hades = {
+  "health" :12,
+  "attackPower" :5,
+  "fireBreath" : 11,
+  "rancidClaw" : 10
+};
+
+let learny = {
+  "health": 12,
+  "attackPower" : 5,
+  "headSmash" : 10,
+  "dropKick" :11
+};
+
+let thesus = {
+  "health" : 12,
+  "attackPower" : 5,
+  "lanceThrust" : 10,
+  "chariotSmash" : 11
+}
