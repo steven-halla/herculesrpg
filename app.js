173b703a06c4d01ce85839dbd99e75e8ee61f7a5
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-08-27 14:01:23 -0700

    "created enemy grab attacks function"

--- app.js
+++ app.js
@@ -1,32 +1,71 @@
 function welcomeAlert() {
-  alert("Welcome to the start of your heroic journey ");
+  alert("Time for some training. Follow the instructions and use lower case for all commands.");
 }
 welcomeAlert();
 
-let hercules = {
+const hercules = {
    "health" : 12,
    "attackPower" : 5,
-   "swordSlash" : 10,
-  "falconPunch" : 11
+  "attacks" : ["Sword Slash", "Falco Punch"]
 };
 
-let hades = {
+const hades = {
   "health" :12,
   "attackPower" :5,
-  "fireBreath" : 11,
-  "rancidClaw" : 10
+  "attacks" : ["Skull Throw", "Laser Blaster"]
 };
 
-let learny = {
+const learny = {
   "health": 12,
   "attackPower" : 5,
-  "headSmash" : 10,
-  "dropKick" :11
+  "attacks" : ["Head Smash", "Fire Breath"]
 };
 
-let thesus = {
+const thesus = {
   "health" : 12,
   "attackPower" : 5,
-  "lanceThrust" : 10,
-  "chariotSmash" : 11
+  "attacks" : ["Lance Thrust", "Divine Favor"]
+};
+
+function battle() {
+  herculesAttacks();
 }
+
+console.log(hercules.health)
+
+function herculesAttacks() {
+  let input = prompt("type in 's' for 'Sword Slash' or 'f' for 'Falco Punch'");
+  if (input === 's') {
+    hercules.attacks[0];
+    console.log("sword slash")
+  } else if (input === 'f') {
+    hercules.attacks[1];
+    console.log("Falcon Punch")
+  } else console.log("wrong command type s or f")
+  (herculesAttacks())
+  return input;
+}
+herculesAttacks();
+
+function battleThesus() {
+  alert(" Time to escape from Hades. Thesus is barring your way out. Time to show him who's the boss. ")
+}
+
+battleThesus();
+
+function getEnemyAttack(array) {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  const item = array[randomIndex];
+  console.log(item);
+  return item;
+}
+
+function thesusAttacks() {
+  const grabAttack = thesus["attacks"];
+   let attack = getEnemyAttack(grabAttack);
+  console.log("you were attacked");
+  // console.log(attack);
+  herculesAttacks();
+  return attack;
+}
+thesusAttacks()
